fec98e406f28d48b18f4b6952cd8007ed55f7c50
Nobuoka Yu <nobuoka@vividcode.info>
2019-05-23 04:44:11 +0900

    Make database not publicly accesible

--- aws/terraform/3-database.tf
+++ aws/terraform/3-database.tf
@@ -27,7 +27,7 @@ resource "aws_db_instance" "db" {
     storage_type = "gp2"
     username = "${var.db_admin_user}"
     password = "${var.db_admin_password}"
-    publicly_accessible = true
+    publicly_accessible = false
     backup_retention_period = 1
     vpc_security_group_ids = ["${aws_security_group.db.id}"]
     db_subnet_group_name = "${aws_db_subnet_group.main.name}"
