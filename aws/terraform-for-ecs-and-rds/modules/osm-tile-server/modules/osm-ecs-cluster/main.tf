01d026fe565603ac3f281182737466924bde34b5
Nobuoka Yu <nobuoka@vividcode.info>
2019-10-23 06:29:54 +0900

    Install CloudWatch Agent to EC2 instance

--- aws/terraform-for-ecs-and-rds/modules/osm-tile-server/modules/osm-ecs-cluster/main.tf
+++ aws/terraform-for-ecs-and-rds/modules/osm-tile-server/modules/osm-ecs-cluster/main.tf
@@ -6,7 +6,9 @@ resource "aws_ecs_cluster" "main" {
 
 data "aws_iam_instance_profile" "ecs_instance_profile" {
   # This IAM Role (and IAM Instance Profile) should be created manually.
-  # https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/instance_IAM_role.html
+  # See : https://docs.aws.amazon.com/AmazonECS/latest/developerguide/instance_IAM_role.html
+  # And CloudWatchAgentServerPolicy should be attached. (for CloudWatch Agent)
+  # See : https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/create-iam-roles-for-cloudwatch-agent-commandline.html
   name = "ecsInstanceRole"
 }
 
@@ -15,6 +17,7 @@ resource "aws_instance" "ecs_container_instance" {
 
   # For ap-northeast-1 region
   # See : https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/launch_container_instance.html
+  # See : https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
   ami = "ami-04a735b489d2a0320"
   instance_type = "t2.large"
   iam_instance_profile = data.aws_iam_instance_profile.ecs_instance_profile.name
@@ -24,6 +27,7 @@ resource "aws_instance" "ecs_container_instance" {
   disable_api_termination = false
   user_data = templatefile("${path.module}/ec2_user_data.template", {
     cluster_name = aws_ecs_cluster.main[0].name
+    cloudwatch_agent_json = file("${path.module}/cloudwatch-agent.json")
   })
   tags = {
     Name = "ECS Container Instance"
