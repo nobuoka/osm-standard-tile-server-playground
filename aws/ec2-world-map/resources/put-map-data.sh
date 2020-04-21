a0b3d47ea7f3d666f2d413f261a8065d083343ef
Nobuoka Yu <nobuoka@vividcode.info>
2020-04-22 04:31:37 +0900

    Update

--- aws/ec2-world-map/resources/put-map-data.sh
+++ aws/ec2-world-map/resources/put-map-data.sh
@@ -1,8 +1,10 @@
 carto_dir=${CARTO_PATH:-"/home/ubuntu/osm/openstreetmap-carto"}
 map_url=${MAP_URL:-"https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf"}
 
+cache_file="/tmp/osm_nodes.cache"
+
 cd ..
-mkdir mapdata
+mkdir -p mapdata
 cd mapdata
 
 wget $map_url -O map.osm.pbf
@@ -14,9 +16,11 @@ wget $map_url -O map.osm.pbf
 sudo -u www-data osm2pgsql -d gis \
   --create --slim -G --hstore --tag-transform-script $carto_dir/openstreetmap-carto.lua \
   -C 2500 --number-processes 1 -S $carto_dir/openstreetmap-carto.style \
-  --flat-nodes ./osm_nodes.cache \
+  --flat-nodes $cache_file \
   --cache-strategy dense \
   ./map.osm.pbf
+
+sudo -u www-data rm $cache_file
 
 # Create indexes
 wget https://raw.githubusercontent.com/gravitystorm/openstreetmap-carto/master/indexes.sql
