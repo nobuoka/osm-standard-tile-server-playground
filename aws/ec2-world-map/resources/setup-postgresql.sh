7c711d1c15fdb329bb8acc2ad11fd5968a7cc514
Nobuoka Yu <nobuoka@vividcode.info>
2020-04-28 19:03:16 +0900

    Update

--- aws/ec2-world-map/resources/setup-postgresql.sh
+++ aws/ec2-world-map/resources/setup-postgresql.sh
@@ -6,4 +6,6 @@ sudo -u postgres psql -c "GRANT \"osm\" TO \"www-data\";"
 
 sudo -u postgres psql -d gis -f init-db.sql
 
-sudo -u postgres cp ./postgres-osm.conf /etc/postgres/10/main/conf.d/
+sudo -u postgres cp ./postgres-osm.conf /etc/postgresql/10/main/conf.d/
+
+sudo service postgresql restart
